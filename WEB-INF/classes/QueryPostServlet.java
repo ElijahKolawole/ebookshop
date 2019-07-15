cff7425b8fbf2d20825effea661abdf1a135d449
Adefemi Kolawole <adefemi.kolawole@hiscox.com>
2019-07-15 14:18:30 -0400

    added post method redirection: essentially I am fetching data using doPost eventhough the doPOst implements doGet internally. The mainb ourpose is to hide the request key -value details from the url

--- WEB-INF/classes/QueryPostServlet.java
+++ WEB-INF/classes/QueryPostServlet.java
@@ -5,13 +5,13 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet("/querymp")   // Configure the request URL for this servlet (Tomcat 7/Servlet 3.0 upwards)
-public class QueryMultiParamServlet extends HttpServlet {
+@WebServlet("/querypost")   // Configure the request URL for this servlet (Tomcat 7/Servlet 3.0 upwards)
+public class QueryPostServlet extends HttpServlet {
 
    // The doGet() runs once per HTTP GET request to this servlet.
    @Override
    public void doGet(HttpServletRequest request, HttpServletResponse response)
-               throws ServletException, IOException {
+   throws ServletException, IOException {
       // Set the MIME type for the response message
       response.setContentType("text/html");
       // Get a output writer to write the response message into the network socket
@@ -24,26 +24,20 @@ public class QueryMultiParamServlet extends HttpServlet {
       try (
          // Step 1: Allocate a database 'Connection' object
          Connection conn = DriverManager.getConnection(
-               "jdbc:mysql://localhost:3306/ebookshop?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+            "jdbc:mysql://localhost:3306/ebookshop?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
                "myuser", "datame1");   // For MySQL
                // The format is: "jdbc:mysql://hostname:port/databaseName", "username", "password"
 
          // Step 2: Allocate a 'Statement' object in the Connection
          Statement stmt = conn.createStatement();
-      ) {
-          // Step 3: Execute a SQL SELECT query
+         ) {
+         // Step 3: Execute a SQL SELECT query
+         String sqlStr = "select * from books where author = "
+               + "'" + request.getParameter("author") + "'"   // Single-quote SQL string
+               + " and qty > 0 order by price desc";
 
-         String sqlStr = "SELECT * FROM books WHERE author = "
-               + "'" + request.getParameter("author") + "'"
-               + " AND price < " + request.getParameter("price")
-               + " AND qty > 0 ORDER BY author ASC, title ASC";
-
-         out.println("<h3>Thank you for your query.</h3>");
+               out.println("<h3>Thank you for your query.</h3>");
          out.println("<p>Your SQL statement is: " + sqlStr + "</p>"); // Echo for debugging
-         if (request.getParameter("author") == null) {
-             out.println("<h2>No author selected. Please go back to select author(s)</h2>");
-             return; // Exit doGet()
-         }
          ResultSet rset = stmt.executeQuery(sqlStr);  // Send the query to the server
 
          // Step 4: Process the query result set
@@ -51,8 +45,8 @@ public class QueryMultiParamServlet extends HttpServlet {
          while(rset.next()) {
             // Print a paragraph <p>...</p> for each record
             out.println("<p>" + rset.getString("author")
-                  + ", " + rset.getString("title")
-                  + ", $" + rset.getDouble("price") + "</p>");
+               + ", " + rset.getString("title")
+               + ", $" + rset.getDouble("price") + "</p>");
             count++;
          }
          out.println("<p>==== " + count + " records found =====</p>");
@@ -61,8 +55,14 @@ public class QueryMultiParamServlet extends HttpServlet {
          out.println("<p>Check Tomcat console for details.</p>");
          ex.printStackTrace();
       }  // Step 5: Close conn and stmt - Done automatically by try-with-resources (JDK 7)
- 
+
       out.println("</body></html>");
       out.close();
    }
+//Use this whenever you already have a getMethod implementation and you dont want to change the wheels. Simply let your doPost call you doGet...
+   @Override
+   public void doPost (HttpServletRequest request, HttpServletResponse response)
+   throws ServletException, IOException {
+   doGet(request, response);  // Re-direct POST request to doGet()
+}
 }
