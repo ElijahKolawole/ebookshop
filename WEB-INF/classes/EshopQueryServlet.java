386acc81668f06a8d5343652fcb04ab4e82d3150
Adefemi Kolawole <adefemi.kolawole@hiscox.com>
2019-07-15 17:34:50 -0400

    created table to display books by author(S), and also created text inpu for buyers details which gets stored in order_records table;

--- WEB-INF/classes/EshopQueryServlet.java
+++ WEB-INF/classes/EshopQueryServlet.java
@@ -5,8 +5,8 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet("/querymv")   // Configure the request URL for this servlet (Tomcat 7/Servlet 3.0 upwards)
-public class QueryMultiValueServlet extends HttpServlet {
+@WebServlet("/eshopquery")   // Configure the request URL for this servlet (Tomcat 7/Servlet 3.0 upwards)
+public class EshopQueryServlet extends HttpServlet {
 
    // The doGet() runs once per HTTP GET request to this servlet.
    @Override
@@ -51,17 +51,48 @@ public class QueryMultiValueServlet extends HttpServlet {
          out.println("<p>Your SQL statement is: " + sqlStr + "</p>"); // Echo for debugging
          ResultSet rset = stmt.executeQuery(sqlStr);  // Send the query to the server
 
-         // Step 4: Process the query result set
-         int count = 0;
+                // Step 4: Process the query result
+         // Print the <form> start tag
+         out.println("<form method='get' action='eshoporder'>");
+         
+         // For each row in ResultSet, print one checkbox inside the <form>
+               
+               out.println("<table border=1 cellpadding='10'>");
+                 out.println("<caption>Books in our invenotory</caption>");
+                 out.println("<thead>");
+                  out.println("<tr> <td></td><td><h2>ID</h2></td><td><h2>AUTHOR</h2></td><td><h2>TITLE</h2></td><td><h2>PRICE</h2></td></tr>");
+                 out.println("</thead>");
+                 
+                     
+                 
+               
+
          while(rset.next()) {
-            // Print a paragraph <p>...</p> for each record
-            out.println("<p>" + rset.getString("id")
-                   + ", " + rset.getString("author")
-                  + ", " + rset.getString("title")
-                  + ", $" + rset.getDouble("price") + "</p>");
-            count++;
+             out.println("<tbody>");
+            // out.println("<p><input type='checkbox' name='id' value="
+            //       + "'" + rset.getString("id") + "' />"
+            //        + rset.getString("id") + ", "
+            //       + rset.getString("author") + ", "
+            //       + rset.getString("title") + ", $"
+            //       + rset.getString("price") + "</p>");
+             out.println("<tr> " + "<td><input type='checkbox' name='id' value='"+rset.getString("id")+"'></td>" + "<td>"+ rset.getString("id") +"</td>" + 
+               "<td>"+ rset.getString("author") + "<td>"+ rset.getString("title") + "<td>"+ rset.getString("price") 
+               +"</td>"+"</td>" +"</td>"+"< /tr>"  );
+            
          }
-         out.println("<p>==== " + count + " records found =====</p>");
+          out.println("</tbody>");
+         out.println("</table>");
+      // Enter Buyer's Information to be stored in the database with the order informtation
+         out.println("<p>Enter your Name: <input type='text' name='cust_name' /></p>");
+         out.println("<p>Enter your Email: <input type='text' name='cust_email' /></p>");
+         out.println("<p>Enter your Phone Number: <input type='text' name='cust_phone' /></p>");
+         
+ 
+         // Print the submit button and </form> end-tag
+         out.println("<p><input type='submit' value='ORDER' />");
+         out.println("</form>");
+
+
       } catch(Exception ex) {
          out.println("<p>Error: " + ex.getMessage() + "</p>");
          out.println("<p>Check Tomcat console for details.</p>");
